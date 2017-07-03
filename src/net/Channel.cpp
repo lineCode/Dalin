3551839eafbbfb7f0773e1ded9bc1ae2843189bc
leohotfn <leohotfn@qq.com>
2017-07-03 20:41:52 +0800

    Update Channel.cpp

--- src/net/Channel.cpp
+++ src/net/Channel.cpp
@@ -5,7 +5,46 @@
 //
 
 #include "Channel.h"
+#include <stdio.h>
+#include <poll.h>
 
 using namespace Xgeer::Net;
 
-Channel::Channel()
+Channel::Channel(EventLoop *loop, int fd)
+ : loop_(loop),
+   fd_(fd),
+   events_(0),
+   revents_(0),
+   index_(-1)
+{
+}
+
+void Channel::update()
+{
+    //TODO
+}
+
+void Channel::handleEvent()
+{
+    if (revents_ & POLLNVAL) {
+        fprintf(stderr, "Channel::handleEvent() POLLNVAL\n");
+    }
+
+    if (revents_ & (POLLERR | POLLNVAL)) {
+        if (errorCallback_) {
+            errorCallback_();
+        }
+    }
+
+    if (revents_ & (POLLIN | POLLPRI | POLLHUP)) {
+        if (readCallback_) {
+            readCallback_();
+        }
+    }
+
+    if (revents_ & POLLOUT) {
+        if (writeCallback_) {
+            writeCallback_();
+        }
+    }
+}
