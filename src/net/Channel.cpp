c983e03b89ece7d8971efb08b2c79e552c247e1e
leohotfn <leohotfn@qq.com>
2017-07-13 11:56:53 +0800

    Update Channel.cpp

--- src/net/Channel.cpp
+++ src/net/Channel.cpp
@@ -10,6 +10,10 @@
 
 using namespace Xgeer::Net;
 
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = POLLIN | POLLPRI;
+const int Channel::kWriteEvent = POLLOUT;
+
 Channel::Channel(EventLoop *loop, int fd)
  : loop_(loop),
    fd_(fd),
