88100c75bc6450d451934ace8b2c385cd8a638ba
leohotfn <leohotfn@qq.com>
2017-07-17 15:26:54 +0800

    Add Noncopyable.h

---
+++ src/base/Noncopyable.h
@@ -0,0 +1,23 @@
+//
+// Noncopyable.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef NONCOPYABLE_H
+#define NONCOPYABLE_H
+
+namespace Xgeer {
+
+class Noncopyable {
+protected:
+    Noncopyable() {}
+    ~Noncopyable() {}
+
+    Noncopyable(const Noncopyable &) = delete;
+    Noncopyable &operator=(const Noncopyable &) = delete;
+};
+
+}
+
+#endif
