d7260167bb306ff01e19fb623b81b9d8c0f6fe68
leohotfn <leohotfn@qq.com>
2017-07-17 13:47:17 +0800

    Add TimerId.h

---
+++ src/net/TimerId.h
@@ -0,0 +1,29 @@
+//
+// TimerId.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef TIMERID_H
+#define TIMERID_H
+
+namespace Xgeer {
+namespace Net {
+
+class Timer;
+
+class TimerId {
+public:
+    explicit TimerId(Timer *timer)
+     : value_(timer)
+    {
+    }
+
+private:
+    Timer *value_;
+};
+
+}
+}
+
+#endif
