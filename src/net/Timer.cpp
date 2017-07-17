49da14945fc28aa7158686eee3801ea84793bac3
leohotfn <leohotfn@qq.com>
2017-07-17 13:46:51 +0800

    Update Timerc.pp

--- src/net/Timer.cpp
+++ src/net/Timer.cpp
@@ -11,7 +11,7 @@ using namespace Xgeer::Detail;
 void Timer::restart(Xgeer::Timestamp now)
 {
     if (repeat_) {
-        expiration_ = addTime(now, interval_);
+        expiration_ = addTime(now, interval_); // ../base/Timestamp.h
     }
     else {
         expiration_ = Timestamp::invalid();
