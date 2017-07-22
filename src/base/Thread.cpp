b90b2f072542e516ba0df23db48bb1c42f85770d
leohotfn <leohotfn@gmail.com>
2017-07-22 17:20:03 +0800

    Add Thread.h, Thread.cpp

---
+++ src/base/Thread.cpp
@@ -0,0 +1,133 @@
+//
+// Thread.cpp
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#include "Thread.h"
+#include "CurrentThread.h"
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <sys/prctl.h>
+#include <exception>
+
+namespace Xgeer {
+namespace CurrentThread {
+
+__thread pid_t t_cachedTid = 0;
+__thread char t_tidString[32];
+__thread int t_tidStringLength = 6;
+__thread const char *t_threadName = "unknown";
+
+void cacheTid()
+{
+    if (t_cachedTid == 0) {
+        t_cachedTid = static_cast<int>(::syscall(SYS_gettid));
+        t_tidStringLength = snprintf(t_tidString, sizeof(t_tidString), "%5d ", t_cachedTid);
+    }
+}
+
+bool isMainThread()
+{
+    return tid() == getpid();
+}
+
+void sleepUsec(int64_t usec)
+{
+    struct timespec ts = {0, 0};
+    int usecPersecond = 1000 * 1000;
+
+    ts.tv_sec = static_cast<time_t>(usec / usecPersecond);
+    ts.tv_nsec = static_cast<long>(usec % usecPersecond * 1000);
+
+    ::nanosleep(&ts, NULL);
+}
+
+}
+
+namespace Detail {
+
+void *startThread(void *arg)
+{
+    Thread *thread = reinterpret_cast<Thread *>(arg);
+    thread->run();
+    return NULL;
+}
+
+}
+
+Thread::Thread(const ThreadFunc &func, const std::string &name)
+ : started_(false),
+   joined_(false),
+   pthreadId_(0),
+   tid_(0),
+   func_(func),
+   name_(name)
+{
+    setDefaultName();
+}
+
+Thread::~Thread()
+{
+    if (started_ && !joined_) {
+        pthread_detach(pthreadId_);
+    }
+}
+
+void Thread::setDefaultName()
+{
+    if (name_.empty()) {
+        char buf[32];
+        snprintf(buf, sizeof(buf), "Thread%d", CurrentThread::tid());
+        name_ = buf;
+    }
+}
+
+void Thread::start()
+{
+    assert(!started_);
+    started_ = true;
+
+    if (pthread_create(&pthreadId_, NULL, Detail::startThread, this)) {
+        started_ = false;
+        fprintf(stderr, "Failed in pthread_create\n");;
+        abort();
+    }
+}
+
+int Thread::join()
+{
+    assert(started_);
+    assert(!joined_);
+    joined_ = true;
+
+    return pthread_join(pthreadId_, NULL);
+}
+
+void Thread::run()
+{
+    tid_ = CurrentThread::tid();
+
+    CurrentThread::t_threadName = name_.empty() ? "XgeerThread" : name_.c_str();
+    ::prctl(PR_SET_NAME, CurrentThread::t_threadName);
+
+    try {
+        func_();
+        CurrentThread::t_threadName = "finished";
+    }
+    catch (std::exception &ex) {
+        CurrentThread::t_threadName = "crashed";
+        fprintf(stderr, "exception caught in Thread %s\n", name_.c_str());
+        fprintf(stderr, "reason: %s\n", ex.what());
+        abort();
+    }
+    catch (...) {
+        CurrentThread::t_threadName = "crashed";
+        fprintf(stderr, "unknown exception caught in Thread %s\n", name_.c_str());
+        throw;
+    }
+}
+
+}
---
+++ src/base/Thread.h
@@ -0,0 +1,45 @@
+//
+// Thread.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <pthread.h>
+#include <string>
+#include <functional>
+#include "Noncopyable.h"
+
+namespace Xgeer {
+
+class Thread : Noncopyable {
+public:
+    typedef std::function<void ()> ThreadFunc;
+
+    explicit Thread(const ThreadFunc&, const std::string &name = std::string());
+    ~Thread();
+
+    void start();
+    int join();
+
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string &name() const { return name_; }
+
+    void run();
+private:
+    void setDefaultName();
+
+    bool started_;
+    bool joined_;
+    pthread_t pthreadId_;
+    pid_t tid_;
+    ThreadFunc func_;
+    std::string name_;
+};
+
+}
+
+#endif
