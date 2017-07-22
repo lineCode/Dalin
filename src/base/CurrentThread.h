7d13507e1a14a47d7bb0a97d77a5fdea5469fbe9
leohotfn <leohotfn@gmail.com>
2017-07-22 17:19:36 +0800

    Add CurrentThread.h

---
+++ src/base/CurrentThread.h
@@ -0,0 +1,53 @@
+//
+// CurrentThread.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef CURRENTTHREAD_H
+#define CURRENTTHREAD_H
+
+#include <pthread.h>
+#include <stdint.h>
+
+namespace Xgeer {
+namespace CurrentThread {
+
+extern __thread pid_t t_cachedTid;
+extern __thread char t_tidString[32];
+extern __thread int t_tidStringLength;
+extern __thread const char *t_threadName;
+
+void cachedTid();
+
+inline pid_t tid()
+{
+    if (t_cachedTid == 0) {
+        cachedTid();
+    }
+    return t_cachedTid;
+}
+
+inline const char *tidString()
+{
+    return t_tidString;
+}
+
+inline int tidStringLength()
+{
+    return t_tidStringLength;
+}
+
+inline const char *threadName()
+{
+    return t_threadName;
+}
+
+bool isMainThread();
+
+void sleepUsec(int64_t usec);
+
+}
+}
+
+#endif
