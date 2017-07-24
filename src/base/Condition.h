b7133a1ee64e5fef79f5991b45ffa580d9317269
leohotfn <leohotfn@gmail.com>
2017-07-24 09:47:04 +0800

    Add Condition.h, COndition.cpp

---
+++ src/base/Condition.h
@@ -0,0 +1,55 @@
+//
+// Condition.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef CONDITION_H
+#define CONDITION_H
+
+#include <pthread.h>
+#include "Mutex.h"
+#include "Noncopyable.h"
+
+namespace Xgeer {
+
+class Condition : Noncopyable {
+public:
+    explicit Condition(MutexLock &mutex)
+     : mutex_(mutex)
+    {
+        MCHECK(pthread_cond_init(&cond_, NULL));
+    }
+
+    ~Condition()
+    {
+        MCHECK(pthread_cond_destroy(&cond_));
+    }
+
+    void wait()
+    {
+        MutexLock::UnassignGuard ug(mutex_);
+        MCHECK(pthread_cond_wait(&cond_, mutex_.getPthreadMutex()));
+    }
+
+    // return true if time out, false otherwise
+    bool waitForSeconds(double seconds);
+
+    void notify()
+    {
+        MCHECK(pthread_cond_signal(&cond_));
+    }
+
+    void notifyAll()
+    {
+        MCHECK(pthread_cond_broadcast(&cond_));
+    }
+
+private:
+    MutexLock &mutex_;
+    pthread_cond_t cond_;
+};
+
+}
+
+#endif
