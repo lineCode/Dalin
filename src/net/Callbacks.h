7ddb7ddf46a2f292ef33a355412d91d4f764a13a
leohotfn <leohotfn@gmail.com>
2017-08-15 17:03:51 +0800

    Add Callbacks.h

---
+++ src/net/Callbacks.h
@@ -0,0 +1,20 @@
+//
+// Callbacks.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+
+#include <functional>
+
+namespace Xgeer {
+namespace Net {
+
+typedef std::function<void ()> TimerCallback;
+
+}
+}
+
+#endif
