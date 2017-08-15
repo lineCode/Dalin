c17e02e930708a5a8979dd306cf6ded1e60115b5
leohotfn <leohotfn@gmail.com>
2017-08-15 17:32:29 +0800

    Add TimerQueue.h, TimerQueue.cpp: Add class TimerQueue

---
+++ src/net/TimerQueue.h
@@ -0,0 +1,19 @@
+//
+// TimerQueue.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef TIMERQUEUE_H
+#define TIMERQUEUE_H
+
+#include <set>
+#include <vector>
+
+#include "../base/Noncopyable.h"
+#include "../base/Timestamp.h"
+#include "../base/Mutex.h"
+#include "Callbacks.h"
+
+
+#endif
