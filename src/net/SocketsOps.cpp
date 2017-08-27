b732f20f745be066f8430baa5b3fded8f6677f5b
leohotfn <leohotfn@gmail.com>
2017-08-27 10:57:24 +0800

    Add SocketsOps.h, SocketsOps.cpp

---
+++ src/net/SocketsOps.cpp
@@ -0,0 +1,130 @@
+//
+// SocketsOps.cpp
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#include "SocketsOps.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+using namespace Dalin;
+
+namespace {
+
+typedef struct sockaddr SA;
+
+const SA* sockaddr_cast(const struct sockaddr_in *addr)
+{
+    return static_cast<const SA*>(reinterpret_cast<const void*>(addr));
+}
+
+SA *sockaddr_cast(struct sockaddr_in *addr)
+{
+    return static_cast<SA*>(reinterpret_cast<void*>(addr));
+}
+
+void setNonblockAndCloseOnExec(int sockfd)
+{
+    // non-block
+    int flags = ::fcntl(sockfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    int ret = ::fcntl(sockfd, F_SETFL, flags);
+
+    // close-on-exec
+    flags = ::fcntl(sockfd, F_GETFD, 0);
+    flags |= FD_CLOEXEC;
+    ret = ::fcntl(sockfd, F_SETFD, 0);
+}
+
+}
+
+int Sockets::createNonblockingOrDie()
+{
+    int sockfd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+    if (sockfd < 0) {
+        fprintf(stderr, "Failed in Sockets::createNonblockingOrDie()\n");
+        abort();
+    }
+
+    return sockfd;
+}
+
+void Sockets::bindOrDie(int sockfd, const struct sockaddr_in &addr)
+{
+    int ret = ::bind(sockfd, sockaddr_cast(&addr), sizeof(addr));
+    if (ret < 0) {
+        fprintf(stderr, "Failed in Sockets::bindOrDie()\n");
+        abort();
+    }
+}
+
+int Sockets::accept(int sockfd, struct sockaddr_in *addr)
+{
+    socklen_t addrlen = sizeof(*addr);
+
+    int connfd = accept4(sockfd, sockaddr_cast(addr), &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
+
+    if (connfd < 0) {
+        int savedErrno = errno;
+        fprintf(stderr, "Failed in Sockets::accept() ");
+        switch (savedErrno) {
+        case EAGAIN:
+        case ECONNABORTED:
+        case EINTR:
+        case EPROTO:
+        case EPERM:
+        case EMFILE:
+            // expected errors
+            errno = savedErrno;
+            break;
+        case EBADF:
+        case EFAULT:
+        case EINVAL:
+        case ENFILE:
+        case ENOBUFS:
+        case ENOMEM:
+        case ENOTSOCK:
+        case EOPNOTSUPP:
+            // unexpected errors
+            fprintf(stderr, "unexpected error: %d\n", savedErrno);
+            abort();
+            break;
+        default:
+            fprintf(stderr, "unknown error: %d\n", savedErrno);
+            abort();
+            break;
+        }
+    }
+
+    return connfd;
+}
+void Sockets::close(int sockfd)
+{
+    if (::close(sockfd) < 0) {
+        fprintf(stderr, "Failed in Sockets::close()\n");
+    }
+}
+
+void Sockets::toHostPort(char *buf, size_t size, const struct sockaddr_in &addr)
+{
+    char host[INET_ADDRSTRLEN] = "INVALID";
+    ::inet_ntop(AF_INET, &addr.sin_addr, host, sizeof(host));
+    uint16_t port = Sockets::networkToHost16(addr.sin_port);
+    snprintf(buf, size, "%s:%u", host, port);
+}
+
+void Sockets::fromHostPort(const char *ip, uint16_t port, struct sockaddr_in *addr)
+{
+    addr->sin_family = AF_INET;
+    addr->sin_port = hostToNetwork16(port);
+    if (::inet_pton(AF_INET, ip, &addr->sin_addr) < 0) {
+        fprintf(stderr, "Failed in Sockets::fromHostPort()\n");
+    }
+}
