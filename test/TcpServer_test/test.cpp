397479f9e859e487f5850773e20f39719bfc2399
leohotfn <leohotfn@gmail.com>
2017-08-27 21:49:50 +0800

    Add TcpServer_test

---
+++ test/TcpServer_test/test.cpp
@@ -0,0 +1,46 @@
+//
+// test.cpp
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#include "../../src/net/Acceptor.h"
+#include "../../src/net/EventLoop.h"
+#include "../../src/net/InetAddress.h"
+#include "../../src/net/TcpServer.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+void onConnection(const Dalin::Net::TcpConnectionPtr &conn)
+{
+    if (conn->connected()) {
+        printf("onConnection(): new connection [%s] from %s\n",
+                conn->name().c_str(), conn->peerAddress().toHostPort().c_str());
+    }
+    else {
+        printf("onConnection(): connection [%s] is down\n", conn->name().c_str());
+    }
+}
+
+void onMessage(const Dalin::Net::TcpConnectionPtr &conn,
+               const char *data,
+               ssize_t len)
+{
+    printf("onMessage(): received %zd bytes from connection [%s]\n", len, conn->name().c_str());
+}
+
+int main()
+{
+    printf("main(): pid = %d\n", getpid());
+
+    Dalin::Net::InetAddress listenAddr(9981);
+    Dalin::Net::EventLoop loop;
+
+    Dalin::Net::TcpServer server(&loop, listenAddr);
+    server.setConnectionCallback(onConnection);
+    server.setMessageCallback(onMessage);
+    server.start();
+
+    loop.loop();
+}
