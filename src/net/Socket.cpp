d80106de09cec92ff1c40f772c29a2773cf7e87f
leohotfn <leohotfn@gmail.com>
2017-08-27 11:25:57 +0800

    Add Socket class

---
+++ src/net/Socket.cpp
@@ -0,0 +1,50 @@
+//
+// Socket.cpp
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#include "Socket.h"
+#include "InetAddress.h"
+#include "SocketsOps.h"
+
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <strings.h>
+
+using namespace Dalin;
+using namespace Dalin::Net;
+
+Socket::~Socket()
+{
+    SocketsOps::close(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress &localAddr)
+{
+    SocketsOps::bindOrDie(sockfd_, localAddr.getSockAddrInet());
+}
+
+void Socket::listen()
+{
+    SocketsOps::listenOrdie(sockfd_);
+}
+
+int Socket::accept(InetAddress *peerAddr)
+{
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+
+    int connfd = SocketsOps::accept(sockfd_, &addr);
+    if (connfd >= 0 ) {
+        peerAddr->setSockAddrInet(addr);
+    }
+
+    return connfd;
+}
+
+void Socket::setReuseAddr(bool on)
+{
+    int optVal = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal));
+}
---
+++ src/net/Socket.h
@@ -0,0 +1,49 @@
+//
+// Socket.h
+//
+// Copyright (c) 2017 Jiawei Feng
+//
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include "../base/Noncopyable.h"
+
+namespace Dalin {
+namespace Net {
+
+class InetAddress;
+
+class Socket : Noncopyable {
+public:
+    explicit Socket(int sockfd)
+     : sockfd_(sockfd)
+    {
+
+    }
+
+    ~Socket();
+
+    int fd() const { return sockfd_; }
+
+    // abort if address in use
+    void bindAddress(const InetAddress &localAddr);
+    // abort if address in use
+    void listen();
+
+    // On success, returns a descriptor for the accepted socket, which has been
+    // set to non-blocking and close-on-exec. *peerAddr is assigned.
+    // On error, -1 is returned, and *peerAddr is untouched.
+    int accept(InetAddress *peerAddr);
+
+    // Enable / disable SO_REUSEADDR
+    void setReuseAddr(bool on);
+
+private:
+    const int sockfd_;
+};
+
+}
+}
+
+#endif
