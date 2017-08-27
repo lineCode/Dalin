6070b09176813dc7a02b3b58d7da015200dd6151
leohotfn <leohotfn@gmail.com>
2017-08-27 16:31:54 +0800

    Update Callbacks.h

--- src/net/Callbacks.h
+++ src/net/Callbacks.h
@@ -8,11 +8,19 @@
 #define CALLBACKS_H
 
 #include <functional>
+#include <memory>
+
+#include "../base/Timestamp.h"
 
 namespace Dalin {
 namespace Net {
 
+class TcpConnection;
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+
 typedef std::function<void ()> TimerCallback;
+typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
+typedef std::function<void (const TcpConnectionPtr&, const char *data, ssize_t len)> MessageCallback;
 
 }
 }
