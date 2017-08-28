f6fc08fb2078cff917c33658919afbe858950945
leohotfn <leohotfn@gmail.com>
2017-08-28 16:41:38 +0800

    Update class TcpConnection

--- src/net/TcpConnection.cpp
+++ src/net/TcpConnection.cpp
@@ -8,6 +8,7 @@
 #include "Channel.h"
 #include "EventLoop.h"
 #include "Socket.h"
+#include "SocketsOps.h"
 
 #include <errno.h>
 #include <stdio.h>
@@ -29,6 +30,9 @@ TcpConnection::TcpConnection(EventLoop *loop,
    peerAddr_(peerAddr)
 {
     channel_->setReadCallback([&](){ this->handleRead(); });
+    channel_->setWriteCallback([&](){ this->handleWrite(); });
+    channel_->setCloseCallback([&](){ this->handleClose(); });
+    channel_->setErrorCallback([&](){ this->handleError(); });
 }
 
 TcpConnection::~TcpConnection()
@@ -47,10 +51,52 @@ void TcpConnection::connectEstablished()
     connectionCallback_(shared_from_this());
 }
 
+void TcpConnection::connectDestroyed()
+{
+    loop_->assertInLoopThread();
+    assert(state_ == kConnected);
+
+    setState(kDisconnected);
+    channel_->disableAll();
+    connectionCallback_(shared_from_this());
+
+    loop_->removeChannel(channel_.get());
+}
+
 void TcpConnection::handleRead()
 {
     char buf[65536];
     ssize_t n = ::read(channel_->fd(), buf, sizeof(buf));
 
-    messageCallback_(shared_from_this(), buf, n);
+    if (n > 0) {
+        messageCallback_(shared_from_this(), buf, n);
+    }
+    else if (n == 0 ) {
+        handleClose();
+    }
+    else {
+        handleError();
+    }
+}
+
+void TcpConnection::handleWrite()
+{
+
+}
+
+void TcpConnection::handleClose()
+{
+    loop_->assertInLoopThread();
+    assert(state_ == kConnected);
+
+    // we don't close fd, leave it to dtor, in order to find leaks easily.
+    channel_->disableAll();
+
+    closeCallback_(shared_from_this());
+}
+
+void TcpConnection::handleError()
+{
+    int err = SocketsOps::getSocketError(channel_->fd());
+    fprintf(stderr, "TcpConnection::handleError(): [%s] - SO_ERROR = %d\n", name_.c_str(), err);
 }
