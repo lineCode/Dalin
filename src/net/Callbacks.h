fb5eae2ccdd292165bdc0a7ab7d8d6d47dbe86a9
leohotfn <leohotfn@gmail.com>
2017-08-28 21:29:03 +0800

    Update Callbacks.h

--- src/net/Callbacks.h
+++ src/net/Callbacks.h
@@ -15,12 +15,13 @@
 namespace Dalin {
 namespace Net {
 
+class Buffer;
 class TcpConnection;
 typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 
 typedef std::function<void ()> TimerCallback;
 typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
-typedef std::function<void (const TcpConnectionPtr&, const char *data, ssize_t len)> MessageCallback;
+typedef std::function<void (const TcpConnectionPtr&, Buffer *buf, Timestamp)> MessageCallback;
 typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
 
 }
