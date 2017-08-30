18b9d371b003eab0805748a1f66c46e785cdc177
leohotfn <leohotfn@gmail.com>
2017-08-30 17:33:37 +0800

    Update EventLoopThreadPool.cpp: fix bug

--- src/net/EventLoopThreadPool.cpp
+++ src/net/EventLoopThreadPool.cpp
@@ -32,9 +32,10 @@ void EventLoopThreadPool::start()
     started_ = true;
 
     for (int i = 0; i < numThreads_; ++i) {
-        auto t = std::unique_ptr<EventLoopThread>();
+        auto t = std::unique_ptr<EventLoopThread>(new EventLoopThread);
+        auto loop = t->startLoop();
         threads_.push_back(std::move(t));
-        loops_.push_back(t->startLoop());
+        loops_.push_back(loop);
     }
 }
 
