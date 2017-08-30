7b2f94585cf745074257276f38754a4921f275b6
leohotfn <leohotfn@gmail.com>
2017-08-30 11:23:49 +0800

    Update Callbacks.h

--- src/net/Callbacks.h
+++ src/net/Callbacks.h
@@ -22,6 +22,7 @@ typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 typedef std::function<void ()> TimerCallback;
 typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
 typedef std::function<void (const TcpConnectionPtr&, Buffer *buf, Timestamp)> MessageCallback;
+typedef std::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
 typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
 
 }
