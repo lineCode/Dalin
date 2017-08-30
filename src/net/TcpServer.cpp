de3127b7c83651f5caf537acb56d76641c8a916c
leohotfn <leohotfn@gmail.com>
2017-08-30 16:47:05 +0800

    Update TcpServer class

--- src/net/TcpServer.cpp
+++ src/net/TcpServer.cpp
@@ -8,6 +8,7 @@
 #include "Acceptor.h"
 #include "EventLoop.h"
 #include "SocketsOps.h"
+#include "EventLoopThreadPool.h"
 
 #include <stdio.h>
 
@@ -34,6 +35,7 @@ TcpServer::TcpServer(EventLoop *loop, const InetAddress &listenAddr)
  : loop_(CHECK_NOTNULL(loop)),
    name_(listenAddr.toHostPort()),
    acceptor_(new Acceptor(loop, listenAddr)),
+   threadPool_(new EventLoopThreadPool(loop)),
    started_(false),
    nextConnId_(1)
 {
@@ -45,10 +47,17 @@ TcpServer::~TcpServer()
 
 }
 
+void TcpServer::setThreadNum(int numThreads)
+{
+    assert(0 <= numThreads);
+    threadPool_->setThreadNum(numThreads);
+}
+
 void TcpServer::start()
 {
     if (!started_) {
         started_ = true;
+        threadPool_->start();
     }
 
     if (!acceptor_->listenning()) {
@@ -66,21 +75,30 @@ void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
     std::string connName = name_ + buf;
 
     InetAddress localAddr(SocketsOps::getLocalAddr(sockfd));
-    TcpConnectionPtr conn(new TcpConnection(loop_, connName, sockfd, localAddr, peerAddr));
+
+    EventLoop *ioLoop = threadPool_->getNextLoop();
+
+    TcpConnectionPtr conn(new TcpConnection(ioLoop, connName, sockfd, localAddr, peerAddr));
     connections_[connName] = conn;
     conn->setConnectionCallback(connectionCallback_);
     conn->setMessageCallback(messageCallback_);
     conn->setWriteCompleteCallback(writeCompleteCallback_);
-    conn->setCloseCallback([&](const TcpConnectionPtr &conn){ this->removeConnection(conn); });
-    conn->connectEstablished();
+    conn->setCloseCallback(std::bind(&TcpServer::removeConnection, this, _1));
+    ioLoop->runInLoop([&](){ conn->connectEstablished(); });
 }
 
 void TcpServer::removeConnection(const TcpConnectionPtr &conn)
 {
+    loop_->runInLoop(std::bind(&TcpServer::removeConnectionInLoop, this, conn));
+}
+
+void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn)
+{
     loop_->assertInLoopThread();
 
-    size_t n = connections_.erase(conn->name());
+    ssize_t n = connections_.erase(conn->name());
     assert(n == 1);
 
-    loop_->queueInLoop([&](){ conn->connectDestroyed(); });
+    EventLoop *ioLoop = conn->getLoop();
+    ioLoop->queueInLoop([&](){ conn->connectDestroyed(); });
 }
--- src/net/TcpServer.h
+++ src/net/TcpServer.h
@@ -20,11 +20,21 @@ namespace Net {
 
 class Acceptor;
 class EventLoop;
+class EventLoopThreadPool;
 
 class TcpServer : Noncopyable {
 public:
     TcpServer(EventLoop *loop, const InetAddress &listenAddr);
     ~TcpServer();
+
+    // Set the number of threads for handling input.
+    // Always accepts new connection in loop's thread.
+    // Must be called before start()!!!
+    // @numThreads
+    // - 0, means all I/O in loop thread, no thread will be created. Default value.
+    // - 1, means all I/O in another thread.
+    // - N, means a thread pool with N threads, new connections are assigned.
+    void setThreadNum(int numThreads);
 
     // Starts the server if it's not listening.
     // Thread safe.
@@ -54,13 +64,17 @@ public:
 private:
     // Not thread safe, but in loop.
     void newConnection(int sockfd, const InetAddress &peerAddr);
+    // Thread safe
     void removeConnection(const TcpConnectionPtr &conn);
+    // Not thread safe, but in loop
+    void removeConnectionInLoop(const TcpConnectionPtr &conn);
 
     typedef std::map<std::string, TcpConnectionPtr> ConnectionMap;
 
     EventLoop *loop_; // the acceptor loop
     const std::string name_;
     std::unique_ptr<Acceptor> acceptor_;
+    std::unique_ptr<EventLoopThreadPool> threadPool_;
     ConnectionCallback connectionCallback_;
     MessageCallback messageCallback_;
     WriteCompleteCallback writeCompleteCallback_;
